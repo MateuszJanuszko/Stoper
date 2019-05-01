da7f4042f4f6e9bdf998998ba1c44203bd62870e
Mateusz <januszkomateusz@gmail.com>
2019-05-01 16:39:30 +0200

    Initial Commit

---
+++ Stoper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stoper
+//
+//  Created by Mateusz Januszko on 01/05/2019.
+//  Copyright © 2019 Mateusz Januszko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
